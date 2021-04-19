c778a04eca75798ac917eb094ff21d3b42303e07
Tomi1026 <77548271+Tomi1026@users.noreply.github.com>
2021-04-19 13:49:30 +0200

    Comparator

---
+++ Comparator/src/Main.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static boolean main (String args[]) {
+		ArrayList<String> ourfiles = new ArrayList<String>(); 
+		ArrayList<String> testfiles = new ArrayList<String>();
+		ArrayList<String> errors = new ArrayList<String>(); 
+
+		try {
+		      File myObj = new File("Output\\"+ args[0]+".json");
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) {
+		        ourfiles.add(myReader.nextLine());
+		      }
+		      myReader.close();
+		      myObj = new File("OutputTest\\"+ args[0]+"_test.json");
+		      Scanner myReader2 = new Scanner(myObj);
+		      while (myReader2.hasNextLine()) {
+		        testfiles.add(myReader2.nextLine());
+		      }
+		      myReader2.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+		boolean failed = false;
+		for(int i = 0; i< ourfiles.size(); i++) {
+			testfiles.set(i, testfiles.get(i).trim());
+			ourfiles.set(i, ourfiles.get(i).trim());
+			if(!ourfiles.get(i).equals(testfiles.get(i))) {
+				errors.add(testfiles.get(i));
+				errors.add(ourfiles.get(i));
+				failed = true;
+			}
+		}
+		
+		if(failed) {
+			System.out.println("A "+ args[0] +" Teszt hibasan futott le!");
+			for(int i = 0; i< errors.size(); i+=2) {
+				System.out.println("Elvárt: " +  errors.get(i) + " \t Kapott: " + errors.get(i+1));
+			}
+			return false;
+		}
+		else {
+			System.out.println("A "+ args[0] +" Teszt sikeresen lefutott!");
+			return true;
+		}
+	}
+}
