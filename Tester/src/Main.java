22523c5e0392c28011926431537c4b411edd8b3b
anna806 <tick.anna@gmail.com>
2021-04-20 13:43:13 +0200

    .

--- Tester/src/Main.java
+++ Tester/src/Main.java
@@ -12,7 +12,6 @@ public class Main {
     	final String dir = System.getProperty("user.dir");
     	File dirf = new File(dir);
     	String parentPath = dirf.getParent();
-    	System.out.println(parentPath);
     	String runProto_parancs;
     	
     	runProto_parancs = "java -jar " + parentPath + "\\proto.jar";
@@ -42,13 +41,10 @@ public class Main {
         	osszesTeszt(parentPath, runProto_parancs, ki);
         }
         else {
-        System.out.println("A lefuttatott parancs: \"" + runProto_parancs + "\"");
-//      process.exec(runProto_parancs).waitFor();
         int exit = process.exec(runProto_parancs).waitFor();
     	System.out.println(exit);
         
         if (ki == 1 && exit != 1) {
-            // TODO: Itt kell a komparátort megkérdezni, jó e a kimenet, argumentumként < kod + "_out" > -ot kell átadni
         	String runComp_parancs = "java -jar " + parentPath + "\\comparator.jar " + kod;
         	int exit2 = process.exec(runComp_parancs).waitFor();
         	if(exit2 == 0 && kod != 0) {
@@ -58,10 +54,6 @@ public class Main {
         		System.out.println(kod + ".teszteset sikertelen\nA hibas sorok:\n");
         		File error = new File(parentPath + "\\Tester\\errors.txt");
         		Scanner myReader = new Scanner(error);
-//        		while(myReader.hasNextLine()) {
-//        			System.out.println("Elvart: " + myReader.nextLine());
-//        			//System.out.println("Kapott: " + myReader.nextLine());
-//        		}
         		int k = 0;
         		while(myReader.hasNextLine()) {
         			if(k %2 == 0)
@@ -78,8 +70,7 @@ public class Main {
         }
         else if(ki == 1 && exit == 1)
         	System.out.println("A teszt nem tudott lefutni");
-        }
-//        System.out.println(runProto_parancs);    
+        } 
     }
     
     public static void osszesTeszt(String parentPath, String runProto_parancs, int ki) {
@@ -102,10 +93,6 @@ public class Main {
     	        		System.out.println((i + 1) + ".teszteset sikertelen\nA hibas sorok:\n");
     	        		File error = new File(parentPath + "\\Tester\\errors.txt");
     	        		Scanner myReader = new Scanner(error);
-    	        		/*while(myReader.hasNextLine()) {
-    	        			System.out.println("Elvart: " + myReader.nextLine());
-    	        			System.out.println("Kapott: " + myReader.nextLine());
-    	        		}*/
     	        		int k = 0;
     	        		while(myReader.hasNextLine()) {
     	        			if(k %2 == 0)
