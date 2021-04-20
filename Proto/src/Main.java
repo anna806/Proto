688387e3ca030ee754d2c35d7029b525489a3f2c
anna806 <tick.anna@gmail.com>
2021-04-20 12:30:37 +0200

    .

--- Proto/src/Main.java
+++ Proto/src/Main.java
@@ -3,11 +3,8 @@ public class Main {
 	public static Jatek game = new Jatek();
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		String[] args1 = {"1", "1", "26"};
-=======
-		String[] args1 = {"1", "1", "21"};
->>>>>>> branch 'master' of https://github.com/anna806/Proto.git
-		game.field(args1);
+		//String[] args1 = {"1", "1", "26"};
+		//game.field(args1);
+		game.field(args);
 	}
 }
