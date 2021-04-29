16c701485f2ee916dac7fd8e7de8643212d60010
anna806 <tick.anna@gmail.com>
2021-04-29 11:00:20 +0200

    JavaFX

--- Proto/src/Main.java
+++ Proto/src/Main.java
@@ -1,8 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
 	public static Jatek game = new Jatek();
 	
 	public static void main(String[] args) {
-		game.field(args);
+		//game.field(args);
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 }
