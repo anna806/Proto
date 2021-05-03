d7988fdef020dc931418e3e8b15ac124a7e6ed4c
boriiza <rozabori@gmail.com>
2021-05-03 22:31:36 +0200

    Menusav főleg

--- Proto/src/grafika/JatekAllapot.java
+++ Proto/src/grafika/JatekAllapot.java
@@ -1,8 +1,14 @@
 package grafika;
 
-public class JatekAllapot {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.layout.Pane;
+
+public class JatekAllapot extends Pane implements EventHandler<ActionEvent>{
 	void felepit(Stage primary, Scene oldscene, int aszam, List<String> nevek) {
-		
+		Jatekter jatekter = new Jatekter(primary, oldscene, aszam, nevek);
+		Menusav menusav = new Menusav(primary, oldscene, aszam, nevek);
+		Muveletsav muveletsav = new Muveletsav(primary, oldscene, aszam, nevek);
 	}
 	void handle(ActionEvent event) {
 		
--- Proto/src/grafika/Menusav.java
+++ Proto/src/grafika/Menusav.java
@@ -1,19 +1,30 @@
 package grafika;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import proto.Aszteroida;
 import proto.Aszteroidaov;
 
-public class Menusav {
+public class Menusav extends Pane implements EventHandler<ActionEvent>{
 	
-	Aszteroidaov jatekter;
-	Jatekter jtk;
+	AszterG aktualis;
+	AszterG valasztott;
 	
 	//FELÉPÍTÉSI TERV
 	/*
@@ -36,75 +47,197 @@ public class Menusav {
 	Button menu;
 	ImageView ment;
 	
-	
-	
+	Label rt1_a;
+   	Label rt1_b;
+   	Label rt2_a;
+   	Label rt2_b;
+   	Label rt3_a;
+   	Label rt3_b;
+   	
+   	Label rb1_a;
+   	ListView rb1_b;
+   	Label rb2_a;
+   	Label rb2_b;
+   	Label rb3_a;
+   	Label rb3_b;
+   	Label rb4_a;
+   	Label rb4_b;
+		
+   	Label gt1_a;
+   	Label gt1_b;
+   	Label gt2_a;
+   	Label gt2_b;
+   	Label gt3_a;
+   	Label gt3_b;
+   	
+   	Label gb1_a;
+   	ListView gb1_b;
+   	Label gb2_a;
+   	Label gb2_b;
+   	Label gb3_a;
+   	Label gb3_b;
+   	Label gb4_a;
+   	Label gb4_b;
+   	
+   	Image mentes;
+   	ImageView mentesView;
+   	
 	public void felepit(Aszteroidaov ov, Jatekter j) {
 		jatekter = ov;
 		jtk = j;
 		
-		primaryStage = new Scene();
+		mentes = new Image("!!!!.png");
+		mentesView = new ImageView();
+		mentesView.setImage(mentes);
 		
-		menu = new Button("Mentes");
+		menu = new Button("Menü");
 		menu.setStyle("-fx-background-color: DARKGOLDENROD");
 		menu.setTextFill(Color.WHITE);
 		menu.setMinSize(70, 45);
 		
-		redBottom = new TableView();
+		//RED TABLE----------------------------------------------
 		
-		///??????
-		TableColumn<String, String> columnT = new TableColumn<>("Telepes");
-	    columnT.setCellValueFactory(new PropertyValueFactory<>("telepes"));
-	    TableColumn<String> columnR = new TableColumn<>("Robot");
-	    columnR.setCellValueFactory(new PropertyValueFactory<>("robot"));
-	    TableColumn<String> columnU = new TableColumn<>("Ufo");
-	    columnU.setCellValueFactory(new PropertyValueFactory<>("ufo"));
-	    TableColumn<String> columnK = new TableColumn<>("Kapu");
-	    columnK.setCellValueFactory(new PropertyValueFactory<>("kapu"));
-
-
-	    redBottom.getColumns().add(columnT);
-	    redBottom.getColumns().add(columnR);
-	    redBottom.getColumns().add(columnU);
-	    redBottom.getColumns().add(columnK);
+		GridPane rootR = new GridPane();
+		 
+	    rootR.setPadding(new Insets(20));
+	    rootR.setHgap(25);
+	    rootR.setVgap(15);
+	 
+	       
+	    rt1_a = new Label("Kéregvastagság");
+	   	rt1_b = new Label("");
+	   	rt2_a = new Label("Napközel");
+	   	rt2_b = new Label("");
+	   	rt3_a = new Label("Napvihar");
+	   	rt3_b = new Label("");
+	   	
+	   	rb1_a = new Label("Telepes");
+	   	rb1_b = new ListView<String>();
+	   	rb2_a = new Label("Robot");
+	   	rb2_b = new Label("");
+	   	rb3_a = new Label("Ufó");
+	   	rb3_b = new Label("");
+	   	rb4_a = new Label("Kapu");
+	   	rb4_b = new Label("");
+	 
+	   	//fenti címek
+	   	GridPane.setHalignment(rt1_a, HPos.LEFT);
+	   	GridPane.setHalignment(rt2_a, HPos.LEFT);
+	   	GridPane.setHalignment(rt3_a, HPos.LEFT);
+	   	rootR.add(rt1_a, 0, 0, 3, 1);
+	   	rootR.add(rt2_a, 0, 1, 3, 1);
+	   	rootR.add(rt3_a, 0, 2, 3, 1);
+	       
+	   	//fenti értékek
+	   	GridPane.setHalignment(rt1_b, HPos.LEFT);
+	   	GridPane.setHalignment(rt2_b, HPos.LEFT);
+	   	GridPane.setHalignment(rt3_b, HPos.LEFT);
+	   	rootR.add(rt1_a, 3, 0);
+	   	rootR.add(rt2_a, 3, 1);
+	   	rootR.add(rt3_a, 3, 2);
+	   	
+	   	//lenti címek
+	   	GridPane.setHalignment(rb1_a, HPos.CENTER);
+	   	GridPane.setHalignment(rb2_a, HPos.CENTER);
+	   	GridPane.setHalignment(rb3_a, HPos.CENTER);
+	   	GridPane.setHalignment(rb4_a, HPos.CENTER);
+	   	rootR.add(rb1_a, 0, 3, 1, 6);
+	   	rootR.add(rb2_a, 1, 3);
+	   	rootR.add(rb3_a, 2, 3);
+	   	rootR.add(rb3_a, 3, 3);
+       
+	   	//lenti értékek
+       
 	    
-	    //milyen object kéne?
-	    redBottom.getItems().add("Bori");
-	    
-
-	    VBox vbox = new VBox(redBottom);
-
-	    Scene scene = new Scene(vbox);
-
-	   
-		primaryStage.setScene(scene);
-
-	    primaryStage.show();
+	  //RED TABLE----------------------------------------------
+	 
+	   	GridPane rootG = new GridPane();
+		 
+	    rootG.setPadding(new Insets(20));
+	    rootG.setHgap(25);
+	    rootG.setVgap(15);
+	       
+	    gt1_a = new Label("Kéregvastagság");
+	   	gt1_b = new Label("");
+	   	gt2_a = new Label("Napközel");
+	   	gt2_b = new Label("");
+	   	gt3_a = new Label("Napvihar");
+	   	gt3_b = new Label("");
+	   	
+	   	gb1_a = new Label("Telepes");
+	   	gb1_b = new ListView<String>();
+	   	gb2_a = new Label("Robot");
+	   	gb2_b = new Label("");
+	   	gb3_a = new Label("Ufó");
+	   	gb3_b = new Label("");
+	   	gb4_a = new Label("Kapu");
+	   	gb4_b = new Label("");
+	 
+	   	//fenti címek
+	   	GridPane.setHalignment(gt1_a, HPos.LEFT);
+	   	GridPane.setHalignment(gt2_a, HPos.LEFT);
+	   	GridPane.setHalignment(gt3_a, HPos.LEFT);
+	   	rootG.add(gt1_a, 0, 0, 3, 1);
+	   	rootG.add(gt2_a, 0, 1, 3, 1);
+	   	rootG.add(gt3_a, 0, 2, 3, 1);
+	       
+	   	//fenti értékek
+	   	GridPane.setHalignment(gt1_b, HPos.LEFT);
+	   	GridPane.setHalignment(gt2_b, HPos.LEFT);
+	   	GridPane.setHalignment(gt3_b, HPos.LEFT);
+	   	rootG.add(gt1_a, 3, 0);
+	   	rootG.add(gt2_a, 3, 1);
+	   	rootG.add(gt3_a, 3, 2);
+	   	
+	   	//lenti címek
+	   	GridPane.setHalignment(gb1_a, HPos.CENTER);
+	   	GridPane.setHalignment(gb2_a, HPos.CENTER);
+	   	GridPane.setHalignment(gb3_a, HPos.CENTER);
+	   	GridPane.setHalignment(gb4_a, HPos.CENTER);
+	   	rootG.add(gb1_a, 0, 3, 1, 6);
+	   	rootG.add(gb2_a, 1, 3);
+	   	rootG.add(gb3_a, 2, 3);
+	   	rootG.add(gb3_a, 3, 3);
+	   	
+	   	
+	       /*Scene scene = new Scene(root, 300, 300);
+	       primaryStage.setTitle("GridPanel Layout Demo (o7planning.org)");
+	       primaryStage.setScene(scene);
+	       primaryStage.show();*/
+	   	
+		
+		
+		
 	}
 
-	@Override
 	public void handle(ActionEvent event) {
-		if(event.getSource() == asas) {
+		if(event.getSource() == ment) {
 			jatekter.getAktual().Furas();
 		}
-		else if(event.getSource() == banyasz) {
-			jatekter.getAktual().Banyaszat();
-			setLabels(jatekter.getAktual());
+	}
+	
+	public void setLabelsRed(Aszteroida a) {
+		rt1_b.setText(String.valueOf(a.getKopenyVastagsag()));
+		if(a.getNapkozel() == "true") {
+			rt2_b.setText("+");
+		} else {
+			rt2_b.setText("-");
 		}
-		else if(event.getSource() == mozog) {
-			jatekter.getAktual().Mozog(jtk.getKivalasztott());	//egérkattintással kiválasztott aszteroida
+		
+		if(a.get == "true") {
+			rt3_b.setText("+");
+		} else {
+			rt3_b.setText("-");
 		}
-		else if(event.getSource() == robotep) {
-			jatekter.getAktual().RobotEpit();	
-		}
-		else if(event.getSource() == kapuep) {
-			jatekter.getAktual().KapuEpit();
-		}
-		else if(event.getSource() == bazisep) {
-			jtk.getKivalasztott().BazisEpit();
-		}
-		else if(event.getSource() == visszatolt) {
-			jatekter.getAktual().Visszatolt();
-			setLabels(jatekter.getAktual());
-		}
+		
+	   	
+
+	   	rb1_b = new ListView(nevek);
+	   	
+	   	
+	   	rb2_b = new Label("");
+
+	   	rb3_b = new Label("");
+	   	rb4_b = new Label("");
 	}
 }
