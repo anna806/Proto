cff17d04c63c096e11c4af4527a12fd7197be66d
anna806 <tick.anna@gmail.com>
2021-05-05 16:36:13 +0200

    Főmenü

--- Proto/grafika/Fomenu.java
+++ Proto/grafika/Fomenu.java
@@ -1,5 +1,6 @@
 package grafika;
 
+import java.io.File;
 import java.util.List;
 
 import javafx.application.Platform;
@@ -11,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
@@ -36,47 +38,51 @@ public class Fomenu implements EventHandler<ActionEvent> {
 	Stage s;
 	Scene scene;
 	
-//	public VBox felepit() {
 	public void felepit(Stage stage) {
 		s = stage;
-		VBox root = new VBox(5);
+		VBox root = new VBox(10);
 		
 		indit  = new Button("Indítás");
-		indit.setStyle("-fx-background-color: DARKGOLDENROD");
+		indit.setStyle("-fx-background-color: DARKGOLDENROD; -fx-font-size: 25");
 		indit.setTextFill(Color.WHITE);
-		indit.setMinSize(140, 60);
+		indit.setMinSize(200, 40);
 		indit.setOnAction(this);
 		
 		folyt  = new Button("Folytatás");
-		folyt.setStyle("-fx-background-color: DARKGOLDENROD");
+		folyt.setStyle("-fx-background-color: DARKGOLDENROD; -fx-font-size: 25");
 		folyt.setTextFill(Color.WHITE);
-		folyt.setMinSize(140, 60);
+		folyt.setMinSize(200, 40);
 		folyt.setOnAction(this);
 		
 		beall  = new Button("Beállítások");
-		beall.setStyle("-fx-background-color: DARKGOLDENROD");
+		beall.setStyle("-fx-background-color: DARKGOLDENROD; -fx-font-size: 25");
 		beall.setTextFill(Color.WHITE);
-		beall.setMinSize(140, 60);
+		beall.setMinSize(200, 40);
 		beall.setOnAction(this);
 		
 		kilep  = new Button("Kilépés");
-		kilep.setStyle("-fx-background-color: DARKGOLDENROD");
+		kilep.setStyle("-fx-background-color: DARKGOLDENROD; -fx-font-size: 25");
 		kilep.setTextFill(Color.WHITE);
-		kilep.setMinSize(140, 60);
+		kilep.setMinSize(200, 40);
 		kilep.setOnAction(this);
 		
 		cim = new Label("Aszteroidabányászat");
 		cim.setStyle("-fx-font-size: 50");
+		cim.setTextFill(Color.WHITE);
 		cim.setTextAlignment(TextAlignment.CENTER);
 		
 		root.getChildren().addAll(cim, indit, folyt, beall, kilep);
 		root.setAlignment(Pos.CENTER);
 		
-//		root.setStyle("-fx-background-color: GOLDENROD");
-		BackgroundImage myBI= new BackgroundImage(new Image("hatter.png",32,32,false,true),
+		String dir = System.getProperty("user.dir");
+    	File dirf = new File(dir);
+    	String parentPath = dirf.getParent();
+		String imagePath = "file:\\" + parentPath + "\\images\\hatter.png";
+		
+		Image image = new Image(imagePath);
+		BackgroundImage myBI= new BackgroundImage(new Image(imagePath, 800, 600, false, true),
 		        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
 		          BackgroundSize.DEFAULT);
-		//then you set to your node
 		root.setBackground(new Background(myBI));
 		
 		scene = new Scene(root, 800, 600);
@@ -89,9 +95,7 @@ public class Fomenu implements EventHandler<ActionEvent> {
 		        Platform.exit();
 		        System.exit(0);
 		    }
-		});
-		
-//		return root;	
+		});	
 	}
 	
 	@Override
