6d66c8c364dfa50be7c68a16daebebc207c37cd2
anna806 <tick.anna@gmail.com>
2021-05-07 19:08:08 +0200

    Jatekter

--- Proto/grafika/Jatekter.java
+++ Proto/grafika/Jatekter.java
@@ -6,58 +6,65 @@ import java.util.Random;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import proto.Aszteroida;
 import proto.Jatek;
+import proto.Teleportkapu;
 
-public class Jatekter {
+public class Jatekter extends Pane{
  //!!!!!! a game átkerült a jatekallpotba
 	//ötletek-Luca
 	private List<AszteroidaView> aszteroidak;
 	private List<KapuView> kapuk;
 	private AszteroidaView kivalasztott;
 	private Jatek game;
+	private Group g;
 	
 	 
-	public void felepit(Jatek _game, Scene _oldscene, Stage _primary){
+	public Group felepit(Jatek _game, Scene _oldscene, Stage _primary){
 		game=_game;
 		int szeles=600;																	// jatekter merete
 		int magas=500;
 		int[] xek;																		//létrehozott aszteroidaviewek x koordinátája
 		int[] yok;																		//y koordinátája
 		List<Aszteroida> bolygok= new ArrayList<Aszteroida>();
-		bolygok=game.GetOv().GetAszteroidak();
+		bolygok=game.GetOv().getAszteroidak();
 		aszteroidak= new ArrayList<AszteroidaView>();
 		kapuk=new ArrayList<KapuView>();
 		List<Teleportkapu> tk= new ArrayList<Teleportkapu>();
 		tk=game.GetOv().GetKapuk();
 		
-		Group g= new Group();
+		g= new Group();
 		Random rand = new Random();
 		
 		for(Aszteroida b : bolygok){
 			int x = rand.nextInt(szeles);
 			int y = rand.nextInt(magas);
-			for(int i=0; i<aszteroidak.size();i++) {
-				if(abs(x-xek.i)<100 && abs(y-yok.i)<100) {					//ha túl közel van,újraszámol, 
+			for(int i=0; i<bolygok.size();i++) {
+				if(Math.abs(x-xek[i])<100 && Math.abs(y-yok[i])<100) {					//ha túl közel van,újraszámol, 
 					x+=110;
 					y+=110;
 				}
 			}
 			AszteroidaView nezet= new AszteroidaView(b, x, y, g);
-			nezet.feltesz();
+			aszteroidak.add(nezet);
+			//nezet.feltesz();
+			g.getChildren().add(nezet.kor);
 		}
-		foreach(Teleportkapu t : tk) {
-			foreach (AszteroidaView av : aszteroidak){
+		for(Teleportkapu t : tk) {
+			for (AszteroidaView av : aszteroidak){
 				if(t.getAszter().equals(av.getAszteroida())) {
 					KapuView uj= new KapuView(t, av.kor.getCenterX()-70, av.kor.getCenterY()-50, g);
 					kapuk.add(uj);
-					uj.feltesz();
+					//uj.feltesz();
+					g.getChildren().add(uj.teglalap);
 				}
 			}
 		}
 		 
 		//kell majd return 
+		return g;
 	}
 	
 	public AszteroidaView getKivalasztott() {
