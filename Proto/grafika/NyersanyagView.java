8109cebe3512a9ad9693649b03e0940aa39c09f8
András Gyenes <gyenes.andras@edu.bme.hu>
2021-05-08 22:57:16 +0200

    NyersanyagView osztály kommentek
    Signed-off-by: András Gyenes <gyenes.andras@edu.bme.hu>

--- Proto/grafika/NyersanyagView.java
+++ Proto/grafika/NyersanyagView.java
@@ -10,11 +10,34 @@ import javafx.stage.Stage;
 
 public class NyersanyagView {
 
+	/**
+	 * A megjelenítéshez szükséges x koordináta
+	 */
 	private int x;
+	
+	/**
+	 * A megjelenítéshez szükséges y koordináta
+	 */
 	private int y;
+	
+	/**
+	 * A nyersanyag adatait tartalmazó adattag, ebben van többek között a megjelenítéshez szükséges típusa
+	 */
 	private Nyersanyag adat;
+	
+	/**
+	 * A nyersanyagot reprezentáló Circle grafikus objektum
+	 */
 	public Circle kor;
 	
+	
+	/**
+	 * A grafikus megjelenítés beállításait kezelõ konstruktor
+	 * 
+	 * @param ny - A megjelenítendõ nyersanyag objektum
+	 * @param kx - A megjelenítéshez szükséges x koordináta
+	 * @param ky - A megjelenítéshez szükséges y koordináta
+	 */
 	public NyersanyagView(Nyersanyag ny, int kx, int ky){
 		if(ny != null)
 			adat=ny;
@@ -29,12 +52,23 @@ public class NyersanyagView {
 		
 	}
 	
+	/**
+	 * Üres aszteroida esetén beállítja a nyersanyag kitöltését az aszteroidáéval megegyezõre
+	 */
 	public void ures() {
 		kor.setFill(Color.DARKSLATEBLUE);
 	}
 	
+	/**
+	 * Betöltött nyersanyag esetén beállítja a kitöltést a megfelelõ színûre
+	 */
 	public void kitolt() {}
 	
+	/**
+	 * Felveszi a nyersanyagot a listára
+	 * 
+	 * @param group - a Group objektum ami a nyersanyagokat tárolja
+	 */
 	public void feltesz(Group group) {
 		group.getChildren().add(kor);
 //		Scene scene= new Scene(group, 41, 41);
