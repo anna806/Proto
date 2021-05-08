d723e89f07c0f86b1ae037332c45921082d1de83
Tomi1026 <77548271+Tomi1026@users.noreply.github.com>
2021-05-08 23:01:04 +0200

    kapu komment

--- Proto/grafika/KapuView.java
+++ Proto/grafika/KapuView.java
@@ -1,21 +1,41 @@
 package grafika;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import proto.*;
 
 public class KapuView extends SzomszedView{
+	/**
+	 * A KapuView-hoz tartozó teleportkapu
+	 */
 	private Teleportkapu kapu;
+	/**
+	 * A Teleportkapu x koordinátája
+	 */
 	double x;
+	/**
+	 * A Teleportkapu x koordinátája
+	 */
 	double y;
+	/**
+	 * A Teleportkaput ábrázoló téglalap
+	 */
 	public Rectangle teglalap;
+	/**
+	 * megadja, hogy ki van-e választva a teleportkapu
+	 */
 	boolean valasztott;
-	Group group;
 	
-	public KapuView(Teleportkapu tk, double kx, double ky, Group g) {
-		group = g;
+	/**
+	 * A kapuView konstruktora, beállítja a paramétereket
+	 * @param tk : A View-hoz tartozó teleportkapu
+	 * @param kx : A kapu x koordinátája
+	 * @param ky : A kapu y koordinátája
+	 */
+	public KapuView(Teleportkapu tk, double kx, double ky) {
 		kapu= new Teleportkapu(); 
 		kapu=tk;
 		x=kx;
@@ -28,6 +48,11 @@ public class KapuView extends SzomszedView{
 		teglalap.setHeight(50);
 		teglalap.setFill(Color.WHITE);
 	}
+	
+	/**
+	 * Beállítja a teleportkapu színét, attók függõen, hogy ki van-e választva
+	 * @param val : megadja, hogy ki van-e választva
+	 */
 	public void SetVal(boolean val) {
 		if(val) {
 			teglalap.setFill(Color.GREEN);
@@ -36,17 +61,30 @@ public class KapuView extends SzomszedView{
 			teglalap.setFill(Color.WHITE);
 	}
 	
+	/**
+	 * Kiszínezi a kapu párját
+	 * @param av : az aszteroida, amin a szomszédja van
+	 */
 	public void SzomszedMutat(AszteroidaView av) {
 		teglalap.setFill(Color.RED);
 		
 	}
-	public void feltesz(Group group) {
-		group.getChildren().add(teglalap);
+	
+	/**
+	 * Kirajzolja a kaput
+	 * @param pane : erre rajzolja a kaput
+	 */
+	public void feltesz(Pane pane) {
+		pane.getChildren().add(teglalap);
 //		Scene scene= new Scene(group, 60, 110);
 //		stage.setScene(scene);
 //		stage.show();
 	}
 	
+	/**
+	 * Visszaadja a View-hoz tartozó teleportkapu
+	 * @return a View-hoz tartozó teleportkapu
+	 */
 	public Teleportkapu getKapu() {
 		return kapu;
 	}
